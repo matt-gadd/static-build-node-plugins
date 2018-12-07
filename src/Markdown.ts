1f0b9a0190bf8e59a343378ccc3ec9e042432aeb
Matt Gadd <mgadd@sitepen.com>
2018-12-07 17:32:30 +0000

    async

--- src/Markdown.ts
+++ src/Markdown.ts
@@ -1,16 +1,39 @@
 import WidgetBase from '@dojo/framework/widget-core/WidgetBase';
 import { v } from '@dojo/framework/widget-core/d';
 import has from '@dojo/framework/has/has';
-import cache from './cache';
+import global from '@dojo/framework/shim/global';
+
+global.__cache = global.__cache || {};
+
+class Cache {
+	private _invalidator: () => void;
+	constructor(invalidator: any) {
+		this._invalidator = invalidator;
+	}
+	get(name: string): any {
+		return global.__cache[name];
+	}
+	set(name: string, value: any): void {
+		if (typeof value.then === 'function') {
+			value.then((result: any) => this.set(name, result));
+		}
+		else {
+			global.__cache[name] = value;
+			this._invalidator();
+		}
+	}
+}
 
 export default class Markdown extends WidgetBase<{ path: string }> {
+	protected cache = new Cache(() => this.invalidate)
+
 	protected render() {
 		const { path } = this.properties;
 		if (has('build-time-render')) {
 			const convert = require('./@build/markdown').default;
-			cache.set(path, convert(path));
+			this.cache.set(path, convert(path));
 		}
-		const text = cache.get(path);
+		const text = this.cache.get(path);
 		return v('div', [ text ]);
 	}
 }
