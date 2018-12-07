0f35d0102def10ed664523135ae7c48c25de1a62
Matt Gadd <mgadd@sitepen.com>
2018-12-07 19:53:21 +0000

    new

--- src/Markdown.ts
+++ src/Markdown.ts
@@ -3,15 +3,15 @@ import { v } from '@dojo/framework/widget-core/d';
 
 export default class Markdown extends WidgetBase<{ path: string }> {
 	private _result: any;
-	private async _markdown(path: string) {
-		const md = await import('./@build/markdown');
-		this._result = await md.default(path);
-		this.invalidate();
-	}
 
 	protected render() {
 		if (!this._result) {
-			this._markdown(this.properties.path);
+			import('./@build/markdown')
+				.then(({ default: md }) => md(this.properties.path))
+				.then((result) => {
+					this._result = result;
+					this.invalidate();
+				});
 		}
 		return v('div', [ this._result ]);
 	}
