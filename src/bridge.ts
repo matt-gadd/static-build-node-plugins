d2cbd1e54b83862b784ad97fe6b5feeba82d689c
Matt Gadd <mgadd@sitepen.com>
2018-12-08 17:10:27 +0000

    work with dist

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,12 +1,13 @@
 import has from '@dojo/framework/has/has';
 declare const modulePath: string;
-const __CACHE__: any = {};
-/** INSERT_CACHE **/
+(window as any).__cache = (window as any).__cache || {};
+(window as any).__cache[modulePath] = {};
+/** @preserve __cache **/
 export default (...args: any[]) => {
 	if (has('build-time-render')) {
 		return (window as any).__communicator(modulePath, args);
 	}
 	else {
-		return __CACHE__[JSON.stringify(args)];
+		return (window as any).__cache[modulePath][JSON.stringify(args)];
 	}
 }
